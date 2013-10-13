507d94c498ccd0954cdeb89e656726906c6c841b
seivadmas <seivadmas@gmail.com>
2013-10-13 15:15:39 -0300

    Initial commit: Project skeleton
    - Starter files from CS106B web page

---
+++ src/ngrams.cpp
@@ -0,0 +1,35 @@
+// This is the CPP file you will edit and turn in.
+// Also remove these comments here and add your own.
+// TODO: remove this comment header
+
+#include <cctype>
+#include <cmath>
+#include <fstream>
+#include <iostream>
+#include <sstream>
+#include <string>
+#include "console.h"
+#include "filelib.h"
+#include "grid.h"
+#include "hashmap.h"
+#include "hashset.h"
+#include "map.h"
+#include "queue.h"
+#include "random.h"
+#include "set.h"
+#include "simpio.h"
+#include "vector.h"
+
+using namespace std;
+
+int main() {
+    setConsoleSize(750, 450);
+    setConsoleFont("Courier New-16");
+    setConsoleEcho(true);
+
+    // TODO: Finish the program!
+
+
+    cout << "Exiting." << endl;
+    return 0;
+}
